7cafea2d52467bb194b9ac36f9102d826a211d0f
Samir Majdi <samir.majdi@edu.uah.es>
2021-04-03 18:39:43 +0200

    Creando la clase Test

---
+++ src/main/java/com/programming/techie/ContactManagerTest.java
@@ -0,0 +1,14 @@
+package com.programming.techie;
+
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ContactManagerTest {
+
+    @Test
+    public void CreandoContacto(){
+
+
+    }
+
+}
